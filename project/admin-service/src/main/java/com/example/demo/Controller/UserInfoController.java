8bc872d7b6eb33bf2b85d4158fe07ad9d9cacdd9
unknown <Gusabary@126.com>
2019-06-10 16:43:22 +0800

    refacot: refactor admin-service

--- project/admin-service/src/main/java/com/example/demo/Controller/UserInfoController.java
+++ project/admin-service/src/main/java/com/example/demo/Controller/UserInfoController.java
@@ -1,55 +1,53 @@
 package com.example.demo.Controller;
+
 import com.alibaba.fastjson.JSONObject;
 import com.example.demo.Service.UserInfoService;
 import com.example.demo.Entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/admin")
-public class UserManage {
+public class UserInfoController {
 
     @Autowired
     private UserInfoService userInfoService;
 
-    @RequestMapping(value = "/Userlist", method = RequestMethod.GET)
-    public JSONObject findAllUser(@RequestParam int pageIndex, @RequestParam int pageSize) {
-        Pageable pageable = PageRequest.of(pageIndex, pageSize);
-        List<User> users = userInfoService.findAll(pageable).getContent();
+    // 返回所有用户信息
+    @RequestMapping(value = "/userlist", method = RequestMethod.GET)
+    public JSONObject findAllUser() {
+        List<User> users = userInfoService.findAll();
         JSONObject result = new JSONObject();
         result.put("users", users);
         return result;
     }
 
-
-    @RequestMapping(value = "/GetTotal", method = RequestMethod.GET)
-    public JSONObject getTotal() {
-        Pageable pageable = PageRequest.of(1, 15);
-        int total = (int) userInfoService.findAll(pageable).getTotalElements();
+    // 返回用户数量
+    @RequestMapping(value = "/totalNum", method = RequestMethod.GET)
+    public JSONObject getTotalNum() {
+        long total = userInfoService.count();
         JSONObject result = new JSONObject();
         result.put("totalNum", total);
         return result;
     }
 
-    @RequestMapping(value = "/ViewDetail", method = RequestMethod.POST)
-    public JSONObject showDetail(@RequestBody JSONObject param){
-        String u_id = param.getString("u_id");
-        User user=userInfoService.queryById(u_id);
+    //返回单个用户信息
+    @RequestMapping(value = "/userinfo", method = RequestMethod.GET)
+    public JSONObject showDetail(@RequestParam(name = "id") String u_id) {
+        User user = userInfoService.queryById(u_id);
         JSONObject result = new JSONObject();
         result.put("user", user);
         return result;
     }
 
-    @RequestMapping(value = "/Manage", method = RequestMethod.PUT)
-    public JSONObject changeStatus(@RequestParam JSONObject param) {
+    // 封禁/解禁用户
+    @RequestMapping(value = "/toggle", method = RequestMethod.PUT)
+    public JSONObject toggleStatus(@RequestParam JSONObject param) {
         String phone = param.getString("phone");
-        User user=userInfoService.queryByPhone(phone);
-        int i= ((user.getStatus()==0)?-1:0);
+        User user = userInfoService.queryByPhone(phone);
+        int i = ((user.getStatus() == 0) ? -1 : 0);   // toggle status
         user.setStatus(i);
         userInfoService.update(user);
         JSONObject result = new JSONObject();
@@ -57,7 +55,8 @@ public class UserManage {
         return result;
     }
 
-    @RequestMapping(value = "/Update", method = RequestMethod.PUT)
+    // 更新用户信息
+    @RequestMapping(value = "/update", method = RequestMethod.PUT)
     public JSONObject update(@RequestBody JSONObject param) {
         String username = param.getString("username");
         String password = param.getString("password");
--- project/admin-service/src/main/java/com/example/demo/Entity/User.java
+++ project/admin-service/src/main/java/com/example/demo/Entity/User.java
@@ -1,22 +1,15 @@
 package com.example.demo.Entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.*;
 
-
-
 @Document(collection = "User")
-@Data
 public class User {
 
     @Id
-    @GeneratedValue
     private String u_id;
+
     private String username;
     private String password;
     private String email;
