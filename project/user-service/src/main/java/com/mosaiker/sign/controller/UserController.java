c0942048047821bad819e99269ad4b8de101ffb3
sjyjytu <1332372004@qq.com>
2019-06-10 14:03:19 +0800

    refactor: 添加了user的注释

--- project/user-service/src/main/java/com/mosaiker/sign/controller/UserController.java
+++ project/user-service/src/main/java/com/mosaiker/sign/controller/UserController.java
@@ -7,6 +7,9 @@ import com.mosaiker.sign.utils.UserUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+/*
+* 用户相关的api
+* */
 @RestController
 @RequestMapping(value = "/api/user")
 public class UserController {
@@ -14,6 +17,11 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    /*
+    * 登录
+    * 输入：phone（Long）
+    * 返回：用户名username，状态status
+    * */
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public JSONObject login(@RequestBody JSONObject params) {
         User user = userService.findUserByPhone(params.getLong("phone"));
@@ -30,6 +38,11 @@ public class UserController {
         return UserUtil.constructJsonOfSuccessfulLogin(user.getUsername(), user.getStatus());
     }
 
+    /*
+     * 注册
+     * 输入：用户名username（String），密码password（String），手机phone（Long）
+     * 返回：注册信息（String）
+     * */
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
     public JSONObject signup(@RequestBody JSONObject params) {
         String result = userService.addUser(
@@ -37,11 +50,21 @@ public class UserController {
         return UserUtil.constructJsonOfMessage(result);
     }
 
+    /*
+     * 激活
+     * 输入：code（String）
+     * 返回：激活信息（String）
+     * */
     @RequestMapping(value = "/activate", method = RequestMethod.GET)
     public JSONObject activate(@RequestParam String code) {
         return UserUtil.constructJsonOfMessage(userService.activate(code));
     }
 
+    /*
+     * 认证
+     * 输入：phone（Long），password（String）
+     * 返回：认证信息（String）
+     * */
     @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
     public JSONObject authenticate(@RequestBody JSONObject request){
         User user = userService.findByPhoneAndPassword(
